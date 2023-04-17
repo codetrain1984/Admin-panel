17008bb5a58bc06558821661695faee1c4c2cce2
gittrain <morezaaa84@gmail.com>
2023-04-18 01:41:55 +0430

    Some charts are created with Recharts and designed via Material UI

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,5 +1,9 @@
+import { Box, Grid, Stack } from '@mui/material'
 import './components/Dashboard.css'
 import DashboardBox from './components/DashboardBox'
+import DashboardChart from './components/DashboardChart'
+import IncomePieChart from './components/IncomePieChart'
+import ExpensePieChart from './components/ExpensePieChart'
 import useDashboard from './hooks/useDashboard'
 
 export default function Dashboard() {
@@ -12,6 +16,26 @@ export default function Dashboard() {
           <DashboardBox key={item.className} {...item} />
         ))}
       </div>
+      <Stack mx={5}>
+        <Grid container my={4} rowSpacing={2} columnSpacing={2}>
+          <Grid item xs={6}>
+            <Box bgcolor="#f7faff" p={2} height="90%">
+              <DashboardChart />
+            </Box>
+          </Grid>
+
+          <Grid item xs={3} sx={{ height: '500px' }}>
+            <Box bgcolor="#f7faff" p={2}>
+              <IncomePieChart />
+            </Box>
+          </Grid>
+          <Grid item xs={3} sx={{ height: '500px' }}>
+            <Box bgcolor="#f7faff" p={2}>
+              <ExpensePieChart />
+            </Box>
+          </Grid>
+        </Grid>
+      </Stack>
     </div>
   )
 }
